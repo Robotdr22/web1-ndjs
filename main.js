8df0d26425153cc2b73251f1b558e72fb9aae248
Robotdr22 <protothread@gmail.com>
2019-02-25 16:28:59 +0900

    homepage undefined

--- main.js
+++ main.js
@@ -6,9 +6,8 @@ var app = http.createServer(function(request,response){
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-  var title = queryData.id;
 
-console.log(url.parse(_url, true));
+
   console.log(url.parse(_url, true).pathname);
   /*if(_url == '/'){
     title = 'Welcome';
@@ -17,30 +16,62 @@ console.log(url.parse(_url, true));
     return response.writeHead(404);
   }*/
   if(pathname === '/'){
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
+    if(queryData.id === undefined){
 
-      response.writeHead(200);
-      response.end(template); ///(queryData.id); //fs.readFileSync(__dirname + _url)
-    });
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+
+        response.writeHead(200);
+        response.end(template); ///(queryData.id); //fs.readFileSync(__dirname + _url)
+      });
+    } else{
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var title = queryData.id;
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+
+        response.writeHead(200);
+        response.end(template); ///(queryData.id); //fs.readFileSync(__dirname + _url)
+      });
+    }
+
   } else{
     response.writeHead(404);
     response.end('Not found');
